8dcaaaa2b7826b46e797f83bb3795274163a0dc3
maathzz <161603184+maathzz@users.noreply.github.com>
2024-03-25 09:24:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,7 @@ for(let i=0;i<botoes.length;i++){
   }
 }
 const contadores=document.querySelectorAll(".contador");
-const tempoObjetivo1=new Date
+const tempoObjetivo1=new Date("2024-03-25T00:00:00:00);
+let tempoAtual=new Date();
+
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
