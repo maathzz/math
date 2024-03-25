b789eee3e1477418887aea8a03eac3fd253ae007
maathzz <161603184+maathzz@users.noreply.github.com>
2024-03-25 09:17:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ for(let i=0;i<botoes.length;i++){
   botoes[i].onclick=function(){
 
     for(let j=0;j<botoes.length;j++{
-      botoes.classlist.remove("ativo");
+      botoes[j].classlist.remove("ativo");
+      textos[j].classlist.remove("ativo");
     }
-    botoes=[i].classlist.add("ativo");
+    botoes=[j].classlist.add("ativo");
+    textos[j].classlist.add("ativo");
   }
 }
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1=new Date
